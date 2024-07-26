926351fc6b2ce4e34f12de8c48d204eebd1084ba
Camilo <c.cneiras@gmail.com>
2024-07-26 14:25:43 -0400

    Commit Inicial

--- __manifest__.py
+++ __manifest__.py
@@ -10,6 +10,11 @@
         'views/stock_location_views.xml',
         'data/ir.model.access.csv',
     ],
+    'assets': {
+        'web.assets_backend': [
+            'stock_location_code/static/src/css/styles.css',
+        ],
+    },
     'installable': True,
     'application': False,
 }
